8f81d4d1ba094ffd6ccd8deb24b0b8e6864cb180
loklok2 <loklok2@naver.com>
2024-05-03 17:50:46 +0900

    3rdweek/Tailwind day1

---
+++ src/03/MyDiv.js
@@ -0,0 +1,9 @@
+
+
+export default function MyDiv() {
+  return (
+    <div className="bg-red-500 w-full text-9xl">
+      Tailwind CSS
+    </div>
+  )
+}
--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import logo from './logo.svg'
 import './App.css';
-import MyClock from './02/MyClock';
+// import MyClock from './02/MyClock';
+import MyDiv from './03/MyDiv';
+import { FaHome } from "react-icons/fa";
 
 function App() {
   return (
-    <div className="App">
-        <MyClock/>
+    <div className="flex flex-col w-full max-w-screen-xl mx-auto h-screen overscroll-y-auto">
+      <header className="flex justify-between items-center text-xl font-bold h-20 p-10 bg-red-500">
+        <p>header</p>
+        <p>K-digital</p>
+        <p><FaHome className="text-3xl text-zinc-200"/></p>
+      </header>
+      <main className='grow'>
+        <div className='flex justify-center items-center'></div>
+      </main>
+      <footer className='flex justify-center items-center text-white bg-slate-800 h-20'>
+        ⓒ 2024 loklok2. All rights reserved.
+      </footer>
     </div>
   );
 }
